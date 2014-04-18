b39b630bfaddb437c622bb21ecc933332bc42231
Ukang'a Dickson <ukanga@gmail.com>
2014-04-18 22:44:23 +0300

    custom zprezto module - auto activate virtulenv and export DJANGO_SETTINGS_MODULE

---
+++ modules/custom/README.md
@@ -0,0 +1,16 @@
+Custom
+======
+
+My custom stuff that makes life easy for me.
+
+
+Django Settings
+---------------
+
+Set DJANGO_SETTINGS_MODULE environment variable to `project_name.preset.local_settings`.
+
+
+Virtualenv
+----------
+
+Activate python virtual environment within the project.
---
+++ modules/custom/init.zsh
@@ -0,0 +1,57 @@
+#
+# My custom stuff that makes life easy for me.
+#
+# Authors:
+#   Ukang'a Dickson <ukanga@gmail.com>
+#
+
+function _workon_cwd()
+{
+    # adapted from oh-my-zsh virtualenvwrapper
+
+    if [ ! $WORKON_CWD ]; then
+        WORKON_CWD=1
+
+        PROJECT_ROOT=`git rev-parse --show-toplevel 2> /dev/null`
+
+        if (( $? != 0 )); then
+            PROJECT_ROOT="."
+        fi
+
+        if [[ "$PROJECT_ROOT" != "." ]]; then
+            ENV_NAME=`basename "$PROJECT_ROOT"`
+        else
+            ENV_NAME=""
+        fi
+
+        if [[ $ENV_NAME != "" ]]; then
+            if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
+                if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
+                    workon $ENV_NAME && export CD_VIRTUAL_ENV="$ENV_NAME"
+                    which python
+                    _local_django_settings_module
+                fi
+            fi
+        elif [ $CD_VIRTUAL_ENV ]; then
+            deactivate && unset CD_VIRTUAL_ENV
+
+            if [ $DJANGO_SETTINGS_MODULE ]; then
+                unset DJANGO_SETTINGS_MODULE
+            fi
+        fi
+
+        unset PROJECT_ROOT
+        unset WORKON_CWD
+    fi
+}
+
+function _local_django_settings_module()
+{
+    if [[ -f "$PROJECT_ROOT/manage.py" ]]; then
+        export DJANGO_SETTINGS_MODULE="$ENV_NAME.preset.local_settings"
+    fi
+}
+
+if (( $+commands[virtualenvwrapper_lazy.sh] )); then
+    add-zsh-hook chpwd _workon_cwd
+fi
